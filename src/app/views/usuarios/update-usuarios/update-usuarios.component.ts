addb1a0ef23132afe9739e0b4320d11a1cbc9c8b
Leonardo Oliveira <leolirarj@yahoo.com.br>
2021-10-19 13:03:57 -0300

    ajustes no update do usuario

--- src/app/views/usuarios/update-usuarios/update-usuarios.component.ts
+++ src/app/views/usuarios/update-usuarios/update-usuarios.component.ts
@@ -20,9 +20,10 @@ export class UpdateUsuariosComponent implements OnInit {
     private formBuilder: FormBuilder,
     private restangular: Restangular,
     private notifierService: NotifierService,
-    private cepService: ConsultaCepService,    
+    private cepService: ConsultaCepService,
   ) {
     this.formulario = this.formBuilder.group({
+      usuarioId: [null, Validators.required],
       nomeCompleto: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
       numeroDocumento: [null, [Validators.required, Validators.minLength(6)]],
       dataNascimento: [null, Validators.required],
@@ -30,43 +31,35 @@ export class UpdateUsuariosComponent implements OnInit {
       genero: [null,[Validators.required]],
       tipoPessoa: [null,[Validators.required]],
       endereco: this.formBuilder.group({
+        enderecoId: [null, Validators.required],
         cep: [null, [Validators.required,  FormValidations.cepValidator]],
         numero: [null, Validators.required],
         complemento: [null, Validators.required],
         bairro: [null, Validators.required],
         cidade: [null, Validators.required],
         estado: [null, Validators.required],
-        logradouro:[null, Validators.required],
-        apelido:[null, Validators.required],
-        pontoReferencia:[null, Validators.required],
-        enderecoPrincipal:[true]
+        logradouro:[null, Validators.required]
       }),
       email:[null,[Validators.required, Validators.email]],
-      senha: [null,[Validators.required]],
       ativo: [null,[Validators.required]],
-      identityId:["null"],
-      returnUrl:["null"],
-      perfil:this.formBuilder.group({
-        descricao:[null],
-        escopos:[null],
-      })
+      perfilId:[null, Validators.required]
     })
    }
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id']
-   console.log(this.id )
+  //  console.log(this.id )
    this.restangular.all('usuario').get( this.id).subscribe(dados => {
-     
-     console.log(dados.data),
+
+    //  console.log(dados.data),
      this.updateForm(dados.data)
    })
   }
 
   onSubmit(){
 
-    console.log(this.formulario.value)
-    console.log(this.id)
+    // console.log(this.formulario.value)
+    // console.log(this.id)
     this.restangular.all('usuario').customPUT(this.formulario.value,  this.id ).subscribe(a => {
       this.notifierService.notify('success', 'Usuário alterado com sucesso');
       this.router.navigateByUrl('/usuarios');
@@ -100,6 +93,7 @@ export class UpdateUsuariosComponent implements OnInit {
   }
   updateForm(dados) {
     this.formulario.patchValue({
+      usuarioId: dados.usuarioId,
       nomeCompleto: dados.nomeCompleto,
       numeroDocumento: dados.numeroDocumento,
       dataNascimento: moment(dados.dataNascimento).format("YYYY-MM-DD"),
@@ -107,31 +101,22 @@ export class UpdateUsuariosComponent implements OnInit {
       genero: dados.genero,
       tipoPessoa: dados.tipoPessoa,
       endereco: {
-        cep: dados.cep,
-        numero: dados.numero,
-        complemento: dados.complemento,
-        bairro: dados.bairro,
-        cidade: dados.cidade,
-        estado: dados.cidade,
-        logradouro: dados.logradouro,
-        apelido: dados.apelido,
-        pontoReferencia: dados.pontoReferencia,
-        
-
-        },
+        enderecoId: dados.endereco ? dados.endereco.enderecoId : 0,
+        cep: dados.endereco ? dados.endereco.cep : '',
+        numero: dados.endereco ? dados.endereco.numero : '',
+        complemento: dados.endereco ? dados.endereco.complemento : '',
+        bairro: dados.endereco ? dados.endereco.bairro : '',
+        cidade: dados.endereco ? dados.endereco.cidade : '',
+        estado: dados.endereco ? dados.endereco.cidade : '',
+        logradouro: dados.endereco ? dados.endereco.logradouro : ''
+      },
       email: dados.email,
-      senha: dados.senha,
       ativo: dados.ativo,
-      identityId: dados.identityId,
-      perfil: {
-      descricao: dados.perfil.descricao,
-      escopos:dados.perfil.escopos
-      }
-      
+      perfilId: dados.perfilId
     })
-    
+
   }
 
-  
+
 
 }
