e867e8dfba4a6c86bcae1c8c97026e57e5052329
IsabellaMoura <70759293+IsabellaMoura@users.noreply.github.com>
2022-03-14 14:49:18 -0300

    img upload Leiloeiro, empresa, comitente

--- src/app/views/comitentes/update-comitente/update-comitente.component.ts
+++ src/app/views/comitentes/update-comitente/update-comitente.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NotifierService } from 'angular-notifier';
 import { Restangular } from 'ngx-restangular';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-update-comitente',
@@ -10,30 +11,32 @@ import { Restangular } from 'ngx-restangular';
   styleUrls: ['./update-comitente.component.scss']
 })
 export class UpdateComitenteComponent implements OnInit {
+  
+  context = {
+    message: 'Hello there!'
+  };
+  imageError: string;
+  isImageSaved: boolean;
+  cardImageBase64: string;
+  
   formulario:FormGroup
   comitente
-  id
+
   public mask: Array<string | RegExp>
   public maskCep: Array<string | RegExp>
   public maskCpf: Array<string | RegExp>
   public maskCnpj: Array<string | RegExp>
+
   constructor(
     private formBuilder: FormBuilder,
     private restangular: Restangular,
     private notifierService: NotifierService,
-    private router: Router,
-    private route: ActivatedRoute,
+    private router: Router
   ) {
-    this.id = this.route.snapshot.params['id']
-
     this.mask = ['(', /[1-9]/, /\d/, ')', ' ', /\d/,/\d/,/\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
     this.maskCep = [ /\d/,/\d/,/\d/,/\d/,/\d/, '-', /\d/, /\d/, /\d/, ]
     this.maskCpf = [ /\d/,/\d/,/\d/,  '.', /\d/,/\d/,/\d/, '.', /\d/, /\d/, /\d/, '-', /\d/,/\d/ ]
     this.maskCnpj = [ /\d/,/\d/,'.',/\d/,/\d/,/\d/,'.',/\d/,/\d/,/\d/,'/', /\d/,/\d/,/\d/,/\d/,'-',/\d/,/\d/, ]
-
-    this.restangular.one("comitente", this.id).get().subscribe((response) => {
-      this.updateForm(response.data);
-     })
 
     this.formulario = this.formBuilder.group({
       cnpj:[null, Validators.required],
@@ -45,40 +48,91 @@ export class UpdateComitenteComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      foto: this.formBuilder.group({
+        arquivoId:[0],
+        nome:[null],
+        base64:[null, Validators.required],
+        tipo:[null],
+        tamanho:[0]
+      }, Validators.required),
+    })
   }
   onSubmit(){
     console.log(this.formulario.value)
-    if(!this.formulario.valid){
-      Object.keys(this.formulario.controls).forEach((campo)=>{
-        const controle = this.formulario.get(campo)
-        controle.markAsTouched()
-
-      })
-      this.notifierService.notify('error', 'Preencha todos os campos obrigatórios');
-      return false;
-    }
-    this.restangular.all('comitente').customPUT(this.formulario.value,  this.id ) .subscribe(a => {
-      this.notifierService.notify('success', 'Comitente editado com sucesso');
+    this.restangular.all('comitente').post(this.formulario.value).subscribe(a => {
+      this.notifierService.notify('success', 'Comitente Criado com sucesso');
       this.router.navigate(['/comitente']);
     },
       error => {
-        this.notifierService.notify('error', 'Erro ao atualizar o Comitente!');
+        this.notifierService.notify('error', 'Erro ao criar o Comitente!');
+
         Object.keys(this.formulario.controls).forEach((campo)=>{
           const controle = this.formulario.get(campo)
           controle.markAsTouched()
         })
       });
   }
-  updateForm(dados){
-    this.formulario.patchValue({
-      cnpj:dados.cnpj,
-      ativo:dados.ativo,
-      comitenteId:dados.comitenteId,
-      dataCadastro:dados.dataCadastro,
-      nome:dados.nome,
-      razaoSocial:dados.razaoSocial,
-    })
+  fileChangeEvent(fileInput: any) {
+    this.imageError = null;
+    if (fileInput.target.files && fileInput.target.files[0]) {
+        // Size Filter Bytes
+        const max_size = 20971520;
+        const allowed_types = ['image/png', 'image/jpeg'];
+        const max_height = 15200;
+        const max_width = 25600;
+
+        if (fileInput.target.files[0].size > max_size) {
+            this.imageError =
+                'O tamanho máximo permitido é ' + max_size / 1000 + 'Mb';
+
+            return false;
+        }
+
+        if (!_.includes(allowed_types, fileInput.target.files[0].type)) {
+            this.imageError = 'Somente imagens são permitidas ( JPG | PNG )';
+            return false;
+        }
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+            const image = new Image();
+            image.src = e.target.result;
+            image.onload = rs => {
+                const img_height = rs.currentTarget['height'];
+                const img_width = rs.currentTarget['width'];
+
+                if (img_height > max_height && img_width > max_width) {
+                    this.imageError =
+                        'Tamanho máximo permitido ' +
+                        max_height +
+                        '*' +
+                        max_width +
+                        'px';
+                    return false;
+                } else {
+                    const imgBase64Path = e.target.result;
+                    this.cardImageBase64 = imgBase64Path;
+                    this.isImageSaved = true;
+                    var foto = this.formulario.get('foto') as FormGroup;
+                    foto.get('base64').setValue(imgBase64Path);
+                    foto.get('nome').setValue(fileInput.target.files[0].name);
+                    foto.get('tamanho').setValue(fileInput.target.files[0].size);
+                    foto.get('tipo').setValue(fileInput.target.files[0].type);
+                    // this.previewImagePath = imgBase64Path;
+                }
+            };
+        };
+
+        reader.readAsDataURL(fileInput.target.files[0]);
+    }
   }
+
+  removeImage(input) {
+    this.cardImageBase64 = null;
+    this.isImageSaved = false;
+    input.value = "";
+  }
+
   verificaValidTouched(campo){
     return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
   }
@@ -86,5 +140,8 @@ export class UpdateComitenteComponent implements OnInit {
   aplicaCssErro(campo){
     return {'has-error': this.verificaValidTouched(campo) }
   }
-
+  onValueChange(event, campo) {
+    this.formulario.get(campo).markAsTouched();
+    this.formulario.get(campo).setValue(event);
+  }
 }
