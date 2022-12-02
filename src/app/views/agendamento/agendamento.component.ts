39bf12c16792ab9a998cd08184c889a84c41cefc
Leonardo Oliveira <leolirarj@yahoo.com.br>
2022-12-01 22:45:10 -0300

    ajustes no agendamento

--- src/app/views/agendamento/agendamento.component.ts
+++ src/app/views/agendamento/agendamento.component.ts
@@ -1,32 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { NotifierService } from 'angular-notifier';
 import { Restangular } from 'ngx-restangular';
 import * as XLSX from 'xlsx';
-import { PdfService } from 'app/_services/pdf.service';
-import { CurrencyFormatPipe } from 'app/directives/currency-format.pipe';
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-agendamento',
   templateUrl: './agendamento.component.html',
   styleUrls: ['./agendamento.component.scss']
 })
 export class AgendamentoComponent implements OnInit {
-  formulario: FormGroup
   loading = true;
   agendamentos;
-  agendamentoNome = 'Status';
-  agendamentosArray;
   agendamentosDefault;
-  data;
+  filtro: FormGroup;
   constructor(
-    private route: ActivatedRoute,
-    private router: Router,
     private restangular: Restangular,
-    private pdfService: PdfService,
-    private currency: CurrencyFormatPipe
+    private formBuilder: FormBuilder
   ) {
-
+    this.filtro =  this.formBuilder.group({
+      data:[null],
+      status:[null]
+    })
    }
 
   ngOnInit() {
@@ -40,103 +35,53 @@ export class AgendamentoComponent implements OnInit {
     )
 
   }
+
   exportAsExcel() {
-    const agendamentosParaEditar = this.agendamentos;
-    const agendamentosModel = agendamentosParaEditar.map( item => {
-    item.campos.map(x => {
-      item = {
-        ...item,
+      const agendamentosModel = this.agendamentos.map(item => {
+        const campos = item.campos.reduce((a, c) => ({...a, [c.campo]:c.valor}), {});
+        item = {
+          "Data Agendamento":item.dataAgendamento,
+          "Leilão":item.leilao,
+          "Numero Lote":item.numeroLote,
+          "Descrição Lote":item.descricaoLote,
+          "Documento Arrematante":item.documentoArrematante,
+          "Nome Arrematante":item.nomeArrematante.toUpperCase(),
+          ...campos
+        }
 
-        [x.campo]:x.valor
-      }
-      return item
-    })
-     item = {
-      ...item,
-        dataAgendamento:item.dataAgendamento,
-        descricaoLote:item.descricaoLote,
-        documentoArrematante:item.documentoArrematante,
-        leilao:item.leilao,
-        localEndereco:item.localEndereco,
-        nomeArrematante:item.nomeArrematante,
-        numeroLote:item.numeroLote,
-      }
-      delete item.campos
-    return item})
+        return item
+      })
 
-    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(agendamentosModel);
+      const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(agendamentosModel);
 
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+      const wb: XLSX.WorkBook = XLSX.utils.book_new();
 
-    XLSX.utils.book_append_sheet(wb, ws, 'Agendamentos');
+      XLSX.utils.book_append_sheet(wb, ws, 'Agendamentos');
 
-    /* save to file */
-    XLSX.writeFile(wb, 'Agendamentos.xlsx');
-}
+      /* save to file */
+      XLSX.writeFile(wb, 'Agendamentos.xlsx');
+  }
 
-exportAsPDF() {
-  let columns = [
-    { title: "Nº Lote ", dataKey: "numeroLote" },
-    { title: "Descrição", dataKey: "descricaoLote" },
-    { title: "Data Agendamento", dataKey: "dataAgendamento" },
-    { title: "Nome", dataKey: "nomeArrematante" },
-    { title: "CPF/CNPJ", dataKey: "documentoArrematante" },
-    { title: "Status", dataKey: "status" },
-    { title: "Placa", dataKey: "placa" },
-    { title: "Chassi", dataKey: "chassi" },
-    { title: "Renavam", dataKey: "renavam" },
+  onValueChange(event, campo) {
+    this.filtro.get(campo).markAsTouched();
+    this.filtro.get(campo).setValue(event);
+    this.setData();
+  }
 
+  setStatus() {
+    const status = this.filtro.controls["status"].value;
+    const agendamentosFiltrados = this.agendamentosDefault.filter(item => item.status === status)
+    this.agendamentos = agendamentosFiltrados
+  }
 
-  ];
-
-  const columStyles = {
-    numeroLote: { columnWidth: 'wrap', overflow: 'visible', halign: 'left', valign: 'middle' },
-    descricaoLote: { columnWidth: 'auto', overflow: 'visible', valign: 'middle', halign: 'left' },
-    dataAgendamento: { columnWidth: 'wrap', overflow: 'visible', halign: 'left', valign: 'middle' },
-    nomeArrematante: { columnWidth: 'wrap', overflow: 'visible', halign: 'left', valign: 'middle' },
-    documentoArrematante: { columnWidth: 'wrap', overflow: 'visible', valign: 'middle', halign: 'left' },
-    status: { columnWidth: 'wrap', overflow: 'linebreak', valign: 'middle', halign: 'left' },
-    placa: { columnWidth: 'wrap', overflow: 'visible', valign: 'middle', halign: 'left' },
-    chassi: { columnWidth: 'wrap', overflow: 'linebreak', valign: 'middle', halign: 'left' },
-    renavam: { columnWidth: 'wrap', overflow: 'linebreak', valign: 'middle', halign: 'left' },
-  };
-
-  const rows = this.agendamentos.map(e => {
-    const placa = e.campos.filter(campo => campo.campo == "Placa")
-    const chassi = e.campos.filter(campo => campo.campo == "Chassi")
-    const renavam = e.campos.filter(campo => campo.campo == "Renavam")
-    return {
-      numeroLote: e.numeroLote || "",
-      descricaoLote: e.descricaoLote || "",
-      dataAgendamento: e.dataAgendamento || "",
-      nomeArrematante: e.nomeArrematante || "",
-      documentoArrematante: e.documentoArrematante || "",
-      status: e.status || "",
-      placa: placa[0].valor || "",
-      chassi: chassi[0].valor || "",
-      renavam:renavam[0].valor || "",
+  setData() {
+    const data = this.filtro.controls["data"].value;
+    this.agendamentos = this.agendamentosDefault;
+    if(data) {
+      const dataFormatada = moment(data).format("DD/MM/YYYY");
+      const agendamentosFiltrados = this.agendamentosDefault.filter(item => moment(item.dataAgendamento, "DD/MM/YYYY HH:mm").format("DD/MM/YYYY") == dataFormatada)
+      this.agendamentos = agendamentosFiltrados
     }
-  });
-
-  const header = {
-    agendamentos: 'Agendamentos'
   }
 
-  this.pdfService.exportPdf('Agendamentos', "Agendamentos", rows, columns, columStyles, header, null);
-}
-setStatus(nome){
-  this.agendamentos = this.agendamentosDefault
-  this.agendamentoNome = nome
-  this.data = '';
-  const agendamentosFiltrados = this.agendamentos.filter(item => item.status == nome)
-  this.agendamentos = agendamentosFiltrados
-}
-setData(dataAgendamento){
-  this.data = dataAgendamento
-  const agendamentosFiltrados = this.agendamentos.filter(item => item.dataAgendamento == dataAgendamento)
-  this.agendamentos = agendamentosFiltrados
-}
-  edit(id) {
-    this.router.navigate(['/edit-listacontatos', id], { relativeTo: this.route });
-  }
 }
