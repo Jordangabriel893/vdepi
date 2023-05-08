32b8fe1b1436f50f95c8041a5fd9b302fd1bfd35
Leonardo Oliveira <leolirarj@yahoo.com.br>
2023-05-08 18:52:37 -0300

    ajustes

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -1,266 +1,245 @@
 export const navigation = [
   {
     title: true,
-    name: 'OPERAÇÃO',
-    grupo:'OPERACAO'
+    name: "OPERAÇÃO",
+    grupo: "OPERACAO",
   },
   {
-    name: 'Leilões',
-    url: '/leilao',
-    icon: 'fa fa-gavel',
-    source: 'LEILOES',
-    grupo:'OPERACAO'
+    name: "Leilões",
+    url: "/leilao",
+    icon: "fa fa-gavel",
+    source: "LEILOES",
+    grupo: "OPERACAO",
   },
   {
-    name: 'Habilitação',
-    url: '/habilitacao',
-    icon: 'fa fa-check',
-    source: 'HABILITACAO',
-    grupo:'OPERACAO'
+    name: "Habilitação",
+    url: "/habilitacao",
+    icon: "fa fa-check",
+    source: "HABILITACAO",
+    grupo: "OPERACAO",
   },
   {
-    name: 'Vistoria',
-    url: '/vistoria',
-    icon: 'fa fa-search',
-    source: 'VISTORIA',
-    grupo:'OPERACAO'
+    name: "Vistoria",
+    url: "/vistoria",
+    icon: "fa fa-search",
+    source: "VISTORIA",
+    grupo: "OPERACAO",
   },
   {
-    name: 'Faturas',
-    url: '/fatura',
-    icon: 'fa fa-file-invoice-dollar',
-    source: 'FATURA',
-    grupo:'OPERACAO'
+    name: "Faturas",
+    url: "/fatura",
+    icon: "fa fa-file-invoice-dollar",
+    source: "FATURA",
+    grupo: "OPERACAO",
   },
   {
-    name: 'Documentos',
-    url: '/gerenciador-documentos',
-    icon: 'fa fa-file-alt',
-    source: 'GERENCIADOR_DOC',
-    grupo:'OPERACAO'
-  },
-  {
-    title: true,
-    name: 'RELATORIOS',
-    grupo:'RELATORIOS'
-  },
-  {
-    name: 'Dashboard',
-    url: '/dashboard',
-    icon: 'fa fa-chart-line',
-    source: 'DASHBOARD',
-    grupo:'RELATORIOS'
-  },
-  {
-    name: 'Mapa de Lances',
-    url: '/mapa-lance',
-    icon: 'fa fa-map',
-    source: 'MAPA_LANCES',
-    grupo:'RELATORIOS'
-  },
-  {
-    name: 'Histórico de Lances',
-    url: '/historicolances',
-    icon: 'fa fa-history',
-    source: 'HISTORICO_LANCES',
-    grupo:'RELATORIOS'
-  },
-   {
-     name: 'Lotes Consolidados',
-     url: '/lancesconsolidado',
-     icon: 'fa fa-file-alt',
-     source: 'LANCES_CONSOLIDADOS',
-     grupo:'RELATORIOS'
-   },
-
-  {
-    name: 'Arrematantes',
-    url: '/arrematantes',
-    icon: 'fa fa-dollar-sign',
-    source: 'ARREMATANTES',
-    grupo:'RELATORIOS'
+    name: "Documentos",
+    url: "/gerenciador-documentos",
+    icon: "fa fa-file-alt",
+    source: "GERENCIADOR_DOC",
+    grupo: "OPERACAO",
   },
   {
     title: true,
-    name: 'MARKETING',
-    grupo:'MARKETING'
+    name: "RELATORIOS",
+    grupo: "RELATORIOS",
   },
   {
-    name: 'Lista de Contatos',
-    url: '/listacontatos',
-    icon: 'fa fa-users',
-    source: 'LISTA_CONTATOS',
-    grupo:'MARKETING'
+    name: "Dashboard",
+    url: "/dashboard",
+    icon: "fa fa-chart-line",
+    source: "DASHBOARD",
+    grupo: "RELATORIOS",
   },
   {
-    name: 'Contatos',
-    url: '/contatos',
-    icon: 'fa fa-user',
-    source: 'CONTATOS',
-    grupo:'MARKETING'
+    name: "Mapa de Lances",
+    url: "/mapa-lance",
+    icon: "fa fa-map",
+    source: "MAPA_LANCES",
+    grupo: "RELATORIOS",
   },
   {
-    name: 'Notificações',
-    url: '/notificacoes',
-    icon: 'fa fa-envelope',
-    source: 'NOTIFICACOES',
-    grupo:'MARKETING'
+    name: "Histórico de Lances",
+    url: "/historicolances",
+    icon: "fa fa-history",
+    source: "HISTORICO_LANCES",
+    grupo: "RELATORIOS",
   },
   {
-    name: 'Meio Notificação',
-    url: '/tipomeionotificao',
-    icon: 'fa fa-envelope-open',
-    source: 'MEIO_NOTIFICACAO',
-    grupo:'MARKETING'
-  },
-  {
-    name: 'Tipo Notificação',
-    url: '/tiponotificacao',
-    icon: 'fa fa-mail-bulk',
-    source: 'TIPO_NOTIFICACAO',
-    grupo:'MARKETING'
-  },
-  {
-    name: 'Agenda',
-    url: '/agenda',
-    icon: 'fa fa-book',
-    source: 'AGENDA',
-    grupo:'MARKETING'
+    name: "Lotes Consolidados",
+    url: "/lancesconsolidado",
+    icon: "fa fa-file-alt",
+    source: "LANCES_CONSOLIDADOS",
+    grupo: "RELATORIOS",
   },
 
   {
-    name: 'Template',
-    url: '/template',
-    icon: 'fa fa-code',
-    source: 'AGENDA',
-    grupo:'MARKETING'
+    name: "Arrematantes",
+    url: "/arrematantes",
+    icon: "fa fa-dollar-sign",
+    source: "ARREMATANTES",
+    grupo: "RELATORIOS",
   },
   {
     title: true,
-    name: 'AGENDAMENTO',
-    grupo:'AGENDAMENTO'
+    name: "MARKETING",
+    grupo: "MARKETING",
   },
   {
-    name: 'Agendamento',
-    url: '/agendamento',
-    icon: 'fa fa-calendar-alt',
-    source: 'AGENDAMENTO',
-    grupo:'MARKETING'
+    name: "Lista de Contatos",
+    url: "/listacontatos",
+    icon: "fa fa-users",
+    source: "LISTA_CONTATOS",
+    grupo: "MARKETING",
   },
   {
-    name: 'Configuração',
-    url: '/configuracao',
-    icon: 'fa fa-cog',
-    source: 'CONFIG_AGENDAMENTO',
-    grupo:'MARKETING'
+    name: "Contatos",
+    url: "/contatos",
+    icon: "fa fa-user",
+    source: "CONTATOS",
+    grupo: "MARKETING",
+  },
+  {
+    name: "Notificações",
+    url: "/notificacoes",
+    icon: "fa fa-envelope",
+    source: "NOTIFICACOES",
+    grupo: "MARKETING",
+  },
+  {
+    name: "Meio Notificação",
+    url: "/tipomeionotificao",
+    icon: "fa fa-envelope-open",
+    source: "MEIO_NOTIFICACAO",
+    grupo: "MARKETING",
+  },
+  {
+    name: "Tipo Notificação",
+    url: "/tiponotificacao",
+    icon: "fa fa-mail-bulk",
+    source: "TIPO_NOTIFICACAO",
+    grupo: "MARKETING",
+  },
+  {
+    name: "Agenda",
+    url: "/agenda",
+    icon: "fa fa-book",
+    source: "AGENDA",
+    grupo: "MARKETING",
+  },
+
+  {
+    name: "Template",
+    url: "/template",
+    icon: "fa fa-code",
+    source: "AGENDA",
+    grupo: "MARKETING",
+  },
+  {
+    title: true,
+    name: "AGENDAMENTO",
+    grupo: "AGENDAMENTO",
+  },
+  {
+    name: "Agendamento",
+    url: "/agendamento",
+    icon: "fa fa-calendar-alt",
+    source: "AGENDAMENTO",
+    grupo: "MARKETING",
+  },
+  {
+    name: "Configuração",
+    url: "/configuracao",
+    icon: "fa fa-cog",
+    source: "CONFIG_AGENDAMENTO",
+    grupo: "MARKETING",
   },
 
   {
     title: true,
-    name: 'GERENCIADOR SITE',
-    grupo:'GERENCIADOR SITE'
+    name: "GERENCIADOR SITE",
+    grupo: "GERENCIADOR SITE",
   },
   {
-    name: 'Banner',
-    url: '/banner',
-    icon: 'fa fa-images',
-    source: 'GERENCIADOR_SITE',
-    grupo:'GERENCIADOR SITE'
+    name: "Banner",
+    url: "/banner",
+    icon: "fa fa-images",
+    source: "GERENCIADOR_SITE",
+    grupo: "GERENCIADOR SITE",
   },
   {
-    name: 'Página Estática',
-    url: '/paginaEstatica',
-    icon: 'fa fa-window-maximize',
-    source: 'GERENCIADOR_SITE',
-    grupo:'GERENCIADOR SITE'
+    name: "Página Estática",
+    url: "/paginaEstatica",
+    icon: "fa fa-window-maximize",
+    source: "GERENCIADOR_SITE",
+    grupo: "GERENCIADOR SITE",
   },
   {
     title: true,
-    name: 'CADASTROS',
-    grupo:'CADASTROS'
+    name: "CADASTROS",
+    grupo: "CADASTROS",
   },
   {
-    name: 'Permissão',
-    url: '/permissao',
-    icon: 'fa fa-user-cog',
-    source: 'PERMISSAO',
-    grupo:'CADASTROS'
+    name: "Permissão",
+    url: "/permissao",
+    icon: "fa fa-user-cog",
+    source: "PERMISSAO",
+    grupo: "CADASTROS",
   },
   {
-    name: 'Usuarios',
-    url: '/usuarios',
-    icon: 'fa fa-user',
-    source: 'USUARIO',
-    grupo:'CADASTROS'
+    name: "Usuarios",
+    url: "/usuarios",
+    icon: "fa fa-user",
+    source: "USUARIO",
+    grupo: "CADASTROS",
   },
   {
-    name: 'Empresas',
-    url: '/empresa',
-    icon: 'fa fa-building',
-    source: 'EMPRESAS',
-    grupo:'CADASTROS'
+    name: "Empresas",
+    url: "/empresa",
+    icon: "fa fa-building",
+    source: "EMPRESAS",
+    grupo: "CADASTROS",
   },
   {
-    name: 'Comitentes',
-    url: '/comitente',
-    icon: 'fa fa-university',
-    source: 'COMITENTES',
-    grupo:'CADASTROS'
+    name: "Comitentes",
+    url: "/comitente",
+    icon: "fa fa-university",
+    source: "COMITENTES",
+    grupo: "CADASTROS",
   },
   {
-    name: 'Leiloeiros',
-    url: '/leiloeiro',
-    icon: 'fa fa-gavel',
-    source: 'LEILOEIROS',
-    grupo:'CADASTROS'
+    name: "Leiloeiros",
+    url: "/leiloeiro",
+    icon: "fa fa-gavel",
+    source: "LEILOEIROS",
+    grupo: "CADASTROS",
   },
   {
-    name: 'Locais',
-    url: '/local',
-    icon: 'fa fa-map-marker',
-    source: 'LOCAIS',
-    grupo:'CADASTROS'
+    name: "Locais",
+    url: "/local",
+    icon: "fa fa-map-marker",
+    source: "LOCAIS",
+    grupo: "CADASTROS",
   },
   {
-    name: 'Categorias',
-    url: '/categorias',
-    icon: 'fa fa-tags',
-    source: 'CATEGORIAS',
-    grupo:'CADASTROS'
-  },
-  // {
-  //   name: 'Status Lote',
-  //   url: '/statusLote',
-  //   icon: 'fa fa-signal',
-  //   source: 'STATUS_LOTE',
-  //   grupo:'CADASTROS'
-  // },
-  // {
-  //   name: 'Status Leilão',
-  //   url: '/statusLeilao',
-  //   icon: 'fa fa-signal',
-  //   source: 'STATUS_LEILAO',
-  //   grupo:'CADASTROS'
-  // },
-  {
-    name: 'Tipo de Documento',
-    url: '/tipodocumento',
-    icon: 'fa fa-file',
-    source: 'GERENCIADOR_DOC',
-    grupo:'CADASTROS'
+    name: "Categorias",
+    url: "/categorias",
+    icon: "fa fa-tags",
+    source: "CATEGORIAS",
+    grupo: "CADASTROS",
   },
   {
-    name: 'Template Documento',
-    url: '/documentotemplate',
-    icon: 'fa fa-code',
-    source: 'GERENCIADOR_DOC',
-    grupo:'CADASTROS'
+    name: "Tipo de Documento",
+    url: "/tipodocumento",
+    icon: "fa fa-file",
+    source: "GERENCIADOR_DOC",
+    grupo: "CADASTROS",
   },
   {
-    name: 'Ger. Documentos',
-    url: '/gerenciadordocumento',
-    icon: 'fa fa-file-alt',
-    source: 'GERENCIADOR_DOC',
-    grupo:'CADASTROS'
+    name: "Template Documento",
+    url: "/documentotemplate",
+    icon: "fa fa-code",
+    source: "GERENCIADOR_DOC",
+    grupo: "CADASTROS",
   },
 ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://hml.id.eblonline.com.br",
-  apiDados: "https://hml.api.eblonline.com.br",
-  // apiUrl: "https://localhost:5001",
-  // apiDados: "https://localhost:4001",
-  siteUrl: "https://localhost:8080"
+  // apiUrl: "https://hml.id.eblonline.com.br",
+  // apiDados: "https://hml.api.eblonline.com.br",
+  apiUrl: "https://localhost:5001",
+  apiDados: "https://localhost:4001",
+  siteUrl: "https://localhost:8080",
   // http://localhost:4200/#/assinatura/94e0a4f1-cf84-4c17-89d5-a869f609b7cc
 };
