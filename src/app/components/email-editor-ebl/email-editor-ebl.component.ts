02d08de8b699fa7184113802a9051d2e230d6fdc
Leonardo Oliveira <leolirarj@yahoo.com.br>
2023-05-10 12:35:53 -0300

    ajustes

--- src/app/components/email-editor-ebl/email-editor-ebl.component.ts
+++ src/app/components/email-editor-ebl/email-editor-ebl.component.ts
@@ -1,12 +1,18 @@
-import { Component, Output, EventEmitter, ViewChild, Input } from '@angular/core';
-import { Restangular } from 'ngx-restangular';
-import { EmailEditorComponent } from 'angular-email-editor';
-import { mergeTags } from '../../views/_models/merge-tags'
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ViewChild,
+  Input,
+} from "@angular/core";
+import { Restangular } from "ngx-restangular";
+import { EmailEditorComponent } from "angular-email-editor";
+import { mergeTags } from "../../views/_models/merge-tags";
 
 @Component({
-  selector: 'app-email-editor-ebl',
-  templateUrl: './email-editor-ebl.component.html',
-  styleUrls: ['./email-editor-ebl.component.scss']
+  selector: "app-email-editor-ebl",
+  templateUrl: "./email-editor-ebl.component.html",
+  styleUrls: ["./email-editor-ebl.component.scss"],
 })
 export class EmailEditorEblComponent {
   @Output() updated = new EventEmitter();
@@ -19,85 +25,81 @@ export class EmailEditorEblComponent {
   private emailEditor: EmailEditorComponent;
 
   cardImageBase64;
-  isImageSaved
+  isImageSaved;
   idEditor = "emaileditor";
   optionsDefault = {
     id: this.idEditor,
-    locale: 'pt-BR',
+    locale: "pt-BR",
     features: {
       textEditor: {
-        tables: true
-      }
+        tables: true,
+      },
     },
     mergeTagsConfig: {
-      autocompleteTriggerChar: "@"
+      autocompleteTriggerChar: "@",
     },
-
   };
   body;
 
-  constructor(
-    private restangular: Restangular,
-  ) {
-    if(this.options) {
+  constructor(private restangular: Restangular) {
+    if (this.options) {
       this.optionsDefault = {
         ...this.optionsDefault,
-        ...this.options
-      }
+        ...this.options,
+      };
     }
 
     this.body = this.bodyValues || {
       backgroundColor: "#ffffff",
       contentWidth: "650px", // or percent "50%"
     };
-
   }
 
   editorReady() {
     this.emailEditor.editor.setBodyValues(this.body);
     this.emailEditor.editor.setMergeTags(mergeTags);
 
-    if(this.designJson){
+    if (this.designJson) {
       this.setDesign();
     }
 
-    const that = this
-    this.emailEditor.editor.addEventListener('design:updated', () => {
-
+    const that = this;
+    this.emailEditor.editor.addEventListener("design:updated", () => {
       //this.salvar = true;
       this.emailEditor.editor.exportHtml((data) => {
         this.updated.emit(data);
-      })
-    })
+      });
+    });
 
-    this.emailEditor.editor.registerCallback('image', function(file, done) {
-      var imagem:any = {
-        base64:'',
-        tipo:'',
-        nome:'',
-      }
+    this.emailEditor.editor.registerCallback("image", function (file, done) {
+      var imagem: any = {
+        base64: "",
+        tipo: "",
+        nome: "",
+      };
 
       const reader = new FileReader();
       reader.onload = (e: any) => {
-          const image = new Image();
-          image.src = e.target.result;
-          image.onload = rs => {
-            const imgBase64Path = e.target.result;
-            that.cardImageBase64 = imgBase64Path;
-            that.isImageSaved = true;
-            imagem.base64 = imgBase64Path
-            imagem.nome = file.attachments[0].name
-            imagem.tipo =file.attachments[0].type
+        const image = new Image();
+        image.src = e.target.result;
+        image.onload = (rs) => {
+          const imgBase64Path = e.target.result;
+          that.cardImageBase64 = imgBase64Path;
+          that.isImageSaved = true;
+          imagem.base64 = imgBase64Path;
+          imagem.nome = file.attachments[0].name;
+          imagem.tipo = file.attachments[0].type;
 
-            that.restangular.all('marketing/imageTemplate')
+          that.restangular
+            .all("marketing/imageTemplate")
             .post(imagem)
-            .subscribe(a => {
-              done({ progress: 100, url: a.data.url })
-            })
-          }
-      }
+            .subscribe((a) => {
+              done({ progress: 100, url: a.data.url });
+            });
+        };
+      };
       reader.readAsDataURL(file.attachments[0]);
-    })
+    });
   }
 
   setDesign() {
--- src/app/views/documento-template/edit-documento-template/edit-documento-template.component.ts
+++ src/app/views/documento-template/edit-documento-template/edit-documento-template.component.ts
@@ -1,51 +1,51 @@
-import { BsModalService } from 'ngx-bootstrap/modal';
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AngularEditorConfig } from '@kolkov/angular-editor';
-import { EmailEditorComponent } from 'angular-email-editor';
-import { NotifierService } from 'angular-notifier';
-import { BsModalRef } from 'ngx-bootstrap';
-import { Restangular } from 'ngx-restangular';
+import { BsModalService } from "ngx-bootstrap/modal";
+import { Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AngularEditorConfig } from "@kolkov/angular-editor";
+import { EmailEditorComponent } from "angular-email-editor";
+import { NotifierService } from "angular-notifier";
+import { BsModalRef } from "ngx-bootstrap";
+import { Restangular } from "ngx-restangular";
 
 @Component({
-  selector: 'app-edit-documento-template',
-  templateUrl: './edit-documento-template.component.html',
-  styleUrls: ['./edit-documento-template.component.scss']
+  selector: "app-edit-documento-template",
+  templateUrl: "./edit-documento-template.component.html",
+  styleUrls: ["./edit-documento-template.component.scss"],
 })
 export class EditDocumentoTemplateComponent implements OnInit {
-  title = 'angular-email-editor';
+  title = "angular-email-editor";
   templateExist = false;
   @ViewChild(EmailEditorComponent)
   private emailEditor: EmailEditorComponent;
   id;
   salvar = false;
   template: any;
-  formulario:FormGroup
+  formulario: FormGroup;
   designJson;
   options = {
-    displayMode: 'web',
-    devices: ['desktop'],
+    displayMode: "web",
+    devices: ["desktop"],
     features: {
       textEditor: {
-        tables: true
-      }
+        tables: true,
+      },
     },
   };
   tools = {
     button: {
-      enabled: false
+      enabled: false,
     },
     form: {
-      enabled: false
+      enabled: false,
     },
     menu: {
-      enabled: false
-    }
+      enabled: false,
+    },
   };
   cardImageBase64;
   isImageSaved;
-  openPopup: boolean = true
+  openPopup: boolean = true;
   modalRef: BsModalRef;
   constructor(
     private modalService: BsModalService,
@@ -53,93 +53,108 @@ export class EditDocumentoTemplateComponent implements OnInit {
     private notifierService: NotifierService,
     private router: Router,
     private restangular: Restangular,
-    private route: ActivatedRoute,
-  ) {
-
-   }
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.id = this.route.snapshot.params['id']
-    this.restangular.all('DocumentoLoteTemplate').get(this.id).subscribe(dados => {
-      this.updateForm(dados.data)
-      this.designJson = JSON.parse(dados.data.designJson);
-    })
+    this.id = this.route.snapshot.params["id"];
+    this.restangular
+      .all("DocumentoLoteTemplate")
+      .get(this.id)
+      .subscribe((dados) => {
+        this.updateForm(dados.data);
+        this.designJson = JSON.parse(dados.data.designJson);
+      });
     this.formulario = this.formBuilder.group({
       titulo: [null, Validators.required],
       html: [null, Validators.required],
-      templateId:[this.id],
-      designJson:[]
-    })
+      templateId: [this.id],
+      designJson: [],
+    });
   }
 
   openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, { class: 'modal-lg'});
+    this.modalRef = this.modalService.show(template, { class: "modal-lg" });
   }
 
   save() {
     this.salvar = true;
 
-    if(!this.formulario.valid) {
-      Object.keys(this.formulario.controls).forEach((campo)=>{
-        const controle = this.formulario.get(campo)
-        controle.markAsTouched()
-      })
-      this.notifierService.notify('error', 'Preencha todos os campos obrigatórios');
-      this.salvar = false
+    if (!this.formulario.valid) {
+      Object.keys(this.formulario.controls).forEach((campo) => {
+        const controle = this.formulario.get(campo);
+        controle.markAsTouched();
+      });
+      this.notifierService.notify(
+        "error",
+        "Preencha todos os campos obrigatórios"
+      );
+      this.salvar = false;
       return false;
     }
 
-    if(!this.template){
-      this.notifierService.notify('success', 'Template atualizado com sucesso');
-      this.router.navigate(['documentotemplate'])
+    if (!this.template) {
+      this.notifierService.notify("success", "Template atualizado com sucesso");
+      this.router.navigate(["documentotemplate"]);
       this.modalRef.hide();
-    }
-    else {
+    } else {
       const template = {
         designJson: this.template.designJson,
         html: this.template.html,
         titulo: this.formulario.value.titulo,
-        templateId:this.id,
-      }
+        templateId: this.id,
+      };
 
-      this.restangular.all('/DocumentoLoteTemplate').customPUT(template, this.id).subscribe(a => {
-        this.salvar = false;
-        this.notifierService.notify('success', 'Template atualizado com sucesso');
-        this.router.navigate(['documentotemplate']);
-      },
-      () => {
-        this.salvar = false;
-        this.notifierService.notify('error', 'Erro ao atualizar o template!');
-      });
+      this.restangular
+        .all("/DocumentoLoteTemplate")
+        .customPUT(template, this.id)
+        .subscribe(
+          (a) => {
+            this.salvar = false;
+            this.notifierService.notify(
+              "success",
+              "Template atualizado com sucesso"
+            );
+            this.router.navigate(["documentotemplate"]);
+          },
+          () => {
+            this.salvar = false;
+            this.notifierService.notify(
+              "error",
+              "Erro ao atualizar o template!"
+            );
+          }
+        );
       this.modalRef.hide();
     }
   }
   updateForm(dados) {
     this.formulario.patchValue({
-      html:dados.html,
+      html: dados.html,
       titulo: dados.titulo,
       templateId: dados.documentoLoteTemplateId,
-      designJson:dados.designJson
-    })
+      designJson: dados.designJson,
+    });
   }
 
   editorUpdated(data) {
     this.template = {
       designJson: JSON.stringify(data.design),
       html: data.html,
-      titulo:this.formulario.value.titulo
-    }
+      titulo: this.formulario.value.titulo,
+    };
   }
 
-  verificaValidTouched(campo){
-    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+  verificaValidTouched(campo) {
+    return (
+      !this.formulario.get(campo).valid && this.formulario.get(campo).touched
+    );
   }
 
-  aplicaCssErro(campo){
-    return{
-      'has-error': this.verificaValidTouched(campo),
-
-    }
+  aplicaCssErro(campo) {
+    return {
+      "has-error": this.verificaValidTouched(campo),
+    };
   }
 
   onValueChange(event, campo) {
