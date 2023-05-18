ee3572e70ff9d6a02723560e97ee419f02c5d26c
Leonardo Oliveira <leolirarj@yahoo.com.br>
2023-05-18 19:10:47 -0300

    ajustes no link da fatura

--- src/app/views/fatura/fatura.component.ts
+++ src/app/views/fatura/fatura.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ConfirmationService, ResolveEmit } from '@jaspero/ng-confirmations';
-import { NotifierService } from 'angular-notifier';
-import { Restangular } from 'ngx-restangular';
-import { Subscription } from 'rxjs';
-import { environment } from 'environments/environment';
-import * as XLSX from 'xlsx';
-import { PdfService } from 'app/_services/pdf.service';
-import { CurrencyFormatPipe } from 'app/directives/currency-format.pipe';
-import { NgIf } from '@angular/common';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ConfirmationService, ResolveEmit } from "@jaspero/ng-confirmations";
+import { NotifierService } from "angular-notifier";
+import { Restangular } from "ngx-restangular";
+import { Subscription } from "rxjs";
+import { environment } from "environments/environment";
+import * as XLSX from "xlsx";
+import { PdfService } from "app/_services/pdf.service";
+import { CurrencyFormatPipe } from "app/directives/currency-format.pipe";
+import { NgIf } from "@angular/common";
 
 @Component({
-  selector: 'app-fatura',
-  templateUrl: './fatura.component.html',
-  styleUrls: ['./fatura.component.scss']
+  selector: "app-fatura",
+  templateUrl: "./fatura.component.html",
+  styleUrls: ["./fatura.component.scss"],
 })
 export class FaturaComponent implements OnInit, OnDestroy {
-  formulario: FormGroup
+  formulario: FormGroup;
   faturas;
   loading;
   sub: Subscription[] = [];
-  nomeLeilao:any = 'Leilões';
+  nomeLeilao: any = "Leilões";
   leilaoId;
-  siteUrl = environment.apiUrl;
+  siteUrl = environment.siteUrl;
   enviando = false;
   faturasEnviar = [];
   todasFaturas = false;
@@ -34,92 +39,123 @@ export class FaturaComponent implements OnInit, OnDestroy {
     private restangular: Restangular,
     private notifierService: NotifierService,
     private confirmationService: ConfirmationService,
-    private formBuilder: FormBuilder) { }
+    private formBuilder: FormBuilder
+  ) {}
 
-   ngOnInit() {
-     this.formulario =  this.formBuilder.group({
-      selectAll:[false],
-      enviarFaturas:this.formBuilder.array([]),
-      statusControl:[null]
-    })
+  ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      selectAll: [false],
+      enviarFaturas: this.formBuilder.array([]),
+      statusControl: [null],
+    });
   }
 
-  setLeilao(idLeilao){
+  setLeilao(idLeilao) {
     this.leilaoId = idLeilao;
     this.loading = true;
     this.sub.push(
-      this.restangular.one(`fatura?leilaoId=${idLeilao.id}`).get().subscribe(
-      dados =>{
-        this.faturas = dados.data;
-        this.faturasFiltradas = dados.data;
-        this.nomeLeilao = idLeilao.nome;
-        this.loading = false
-        this.formulario = this.formBuilder.group({
-          enviarFaturas:this.formBuilder.array(dados.data ? dados.data.map(x => this.formBuilder.group({ faturaId:x.faturaId, valor: false })) : [], Validators.required),
-          selectAll:[false],
-          statusControl:[0]
+      this.restangular
+        .one(`fatura?leilaoId=${idLeilao.id}`)
+        .get()
+        .subscribe((dados) => {
+          this.faturas = dados.data;
+          this.faturasFiltradas = dados.data;
+          this.nomeLeilao = idLeilao.nome;
+          this.loading = false;
+          this.formulario = this.formBuilder.group({
+            enviarFaturas: this.formBuilder.array(
+              dados.data
+                ? dados.data.map((x) =>
+                    this.formBuilder.group({
+                      faturaId: x.faturaId,
+                      valor: false,
+                    })
+                  )
+                : [],
+              Validators.required
+            ),
+            selectAll: [false],
+            statusControl: [0],
+          });
         })
-      }
-    )
-    )
+    );
   }
 
   cancelarFatura(faturaId) {
     this.sub.push(
-    this.confirmationService.create('Atenção', 'Deseja realmente cancelar esta fatura?')
+      this.confirmationService
+        .create("Atenção", "Deseja realmente cancelar esta fatura?")
         .subscribe((ans: ResolveEmit) => {
-          if(ans.resolved) {
+          if (ans.resolved) {
             this.sub.push(
-              this.restangular.all(`fatura/cancelar/${faturaId}`).post().subscribe(() => {
-                this.notifierService.notify('success', 'Fatura Cancelada com sucesso');
-                this.setLeilao(this.leilaoId);
-              },
-              () => {
-                this.notifierService.notify('error', 'Erro ao cancelar fatura');
-              })
+              this.restangular
+                .all(`fatura/cancelar/${faturaId}`)
+                .post()
+                .subscribe(
+                  () => {
+                    this.notifierService.notify(
+                      "success",
+                      "Fatura Cancelada com sucesso"
+                    );
+                    this.setLeilao(this.leilaoId);
+                  },
+                  () => {
+                    this.notifierService.notify(
+                      "error",
+                      "Erro ao cancelar fatura"
+                    );
+                  }
+                )
             );
           }
         })
     );
   }
 
-  sendFatura(){
-    if(this.faturasEnviar.length === 0) {
-      this.notifierService.notify('error', 'Selecione alguma fatura para enviar');
+  sendFatura() {
+    if (this.faturasEnviar.length === 0) {
+      this.notifierService.notify(
+        "error",
+        "Selecione alguma fatura para enviar"
+      );
       return false;
     }
 
     this.enviando = true;
 
     const body = {
-      faturaIds: this.faturasEnviar
+      faturaIds: this.faturasEnviar,
     };
 
-    this.restangular.all("fatura/notificar").post(body).subscribe(() => {
-      this.notifierService.notify('success', 'Faturas Enviadas');
-      this.setLeilao(this.leilaoId);
-      this.enviando = false;
-    },
-    () => {
-      this.notifierService.notify('error', 'Erro ao enviar faturas');
-      this.enviando = false;
-    })
+    this.restangular
+      .all("fatura/notificar")
+      .post(body)
+      .subscribe(
+        () => {
+          this.notifierService.notify("success", "Faturas Enviadas");
+          this.setLeilao(this.leilaoId);
+          this.enviando = false;
+        },
+        () => {
+          this.notifierService.notify("error", "Erro ao enviar faturas");
+          this.enviando = false;
+        }
+      );
   }
 
-  selectAllFaturas(){
-   this.todasFaturas = !this.todasFaturas;
-   this.faturasEnviar = [];
-   if(this.todasFaturas) {
-    this.faturasEnviar = this.faturas.map(x => x.faturaId);
-   }
+  selectAllFaturas() {
+    this.todasFaturas = !this.todasFaturas;
+    this.faturasEnviar = [];
+    if (this.todasFaturas) {
+      this.faturasEnviar = this.faturas.map((x) => x.faturaId);
+    }
   }
 
   selectFatura(faturaId) {
     const idx = this.faturasEnviar.indexOf(faturaId);
-    if(idx >= 0) {
+    if (idx >= 0) {
       this.faturasEnviar = this.faturasEnviar.splice(idx, 1);
-    }
-    else {
+    } else {
       this.faturasEnviar.push(faturaId);
     }
   }
@@ -128,38 +164,45 @@ export class FaturaComponent implements OnInit, OnDestroy {
 
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
 
-    XLSX.utils.book_append_sheet(wb, ws, 'Faturas');
+    XLSX.utils.book_append_sheet(wb, ws, "Faturas");
 
     /* save to file */
-    XLSX.writeFile(wb, 'Faturas.xlsx');
-}
-
-  onSearch(){
-      let value = this.queryField.value.toLowerCase();
-
-      this.faturasFiltradas =
-        this.faturas.filter(x => x.nomeArrematante.toLowerCase().includes(value) ||
-                                  x.cpfArrematante.includes(value) ||
-                                  x.cpfArrematante.replace('.', '').replace('-', '').replace('/', '').includes(value) ||
-                                  x.itens.some(i => i.descricao.toLowerCase().includes(value))
-                                  );
+    XLSX.writeFile(wb, "Faturas.xlsx");
   }
 
-  filtrarFaturas(){
-    const statusControl = this.formulario.value.statusControl
-    if(statusControl == '0'){
-      this.faturasFiltradas = this.faturas
-      return
+  onSearch() {
+    let value = this.queryField.value.toLowerCase();
+
+    this.faturasFiltradas = this.faturas.filter(
+      (x) =>
+        x.nomeArrematante.toLowerCase().includes(value) ||
+        x.cpfArrematante.includes(value) ||
+        x.cpfArrematante
+          .replace(".", "")
+          .replace("-", "")
+          .replace("/", "")
+          .includes(value) ||
+        x.itens.some((i) => i.descricao.toLowerCase().includes(value))
+    );
+  }
+
+  filtrarFaturas() {
+    const statusControl = this.formulario.value.statusControl;
+    if (statusControl == "0") {
+      this.faturasFiltradas = this.faturas;
+      return;
     }
 
-    this.faturasFiltradas =  this.faturasFiltradas.filter(fatura => fatura.status == statusControl);
+    this.faturasFiltradas = this.faturasFiltradas.filter(
+      (fatura) => fatura.status == statusControl
+    );
   }
 
-  showItensFatura(faturaId){
+  showItensFatura(faturaId) {
     this.showItens = faturaId;
   }
 
   ngOnDestroy(): void {
-    this.sub.forEach(s => s.unsubscribe())
+    this.sub.forEach((s) => s.unsubscribe());
   }
 }
