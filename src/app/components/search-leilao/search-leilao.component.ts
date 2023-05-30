00e3d3aa38e8aa25a3d0d51248058697778d707f
Leonardo Oliveira <leolirarj@yahoo.com.br>
2023-05-30 19:47:09 -0300

    ajustes no encerramento do leilão

--- src/app/components/search-leilao/search-leilao.component.ts
+++ src/app/components/search-leilao/search-leilao.component.ts
@@ -1,50 +1,62 @@
-import { Component, OnDestroy, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import * as Model from '../../views/_models/model'
-import { Restangular } from 'ngx-restangular';
-import { Subscription } from 'rxjs';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  Output,
+  EventEmitter,
+} from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import * as Model from "../../views/_models/model";
+import { Restangular } from "ngx-restangular";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-search-leilao',
-  templateUrl: './search-leilao.component.html',
-  styleUrls: ['./search-leilao.component.scss']
+  selector: "app-search-leilao",
+  templateUrl: "./search-leilao.component.html",
+  styleUrls: ["./search-leilao.component.scss"],
 })
 export class SearchLeilaoComponent implements OnInit, OnDestroy {
   @Output() emitLeilao = new EventEmitter();
-  formulario: FormGroup
+  formulario: FormGroup;
   leiloes: Model.Leilao[];
   loading = true;
-  selectLeilao
+  selectLeilao;
   sub: Subscription[] = [];
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private restangular: Restangular,
-    private formBuilder: FormBuilder,
-  ) { }
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.formulario = this.formBuilder.group({
-      leilao:[null]
-    })
+      leilao: [null],
+    });
 
-    this.sub.push( this.restangular.all("admin").one("leilao").get().subscribe((response) => {
-      this.selectLeilao = response.data
-      this.leiloes = response.data;
-      this.leiloes.sort(function(a,b) {
-        return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
-    })
-      this.loading = false;
-    },
-    () => this.loading = false)
-    )
+    this.sub.push(
+      this.restangular
+        .all("admin")
+        .one("leilao")
+        .get()
+        .subscribe(
+          (response) => {
+            this.selectLeilao = response.data;
+            this.leiloes = response.data;
+            //   this.leiloes.sort(function(a,b) {
+            //     return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
+            // })
+            this.loading = false;
+          },
+          () => (this.loading = false)
+        )
+    );
   }
-  emitirLeilao(){
-    this.emitLeilao.emit(this.formulario.value.leilao)
+  emitirLeilao() {
+    this.emitLeilao.emit(this.formulario.value.leilao);
   }
   ngOnDestroy(): void {
-    this.sub.forEach(s => s.unsubscribe())
+    this.sub.forEach((s) => s.unsubscribe());
   }
-
 }
