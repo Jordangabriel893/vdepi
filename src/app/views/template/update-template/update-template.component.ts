6dac76b6a581482122972fb528b26da3eddda788
Leonardo Oliveira <leolirarj@yahoo.com.br>
2023-10-26 11:08:27 -0300

    ajuste na atualização do template

--- src/app/views/template/update-template/update-template.component.ts
+++ src/app/views/template/update-template/update-template.component.ts
@@ -8,10 +8,9 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-update-template',
   templateUrl: './update-template.component.html',
-  styleUrls: ['./update-template.component.scss']
+  styleUrls: ['./update-template.component.scss'],
 })
 export class UpdateTemplateComponent implements OnInit {
-
   title = 'angular-email-editor';
   templateExist = false;
   @ViewChild(EmailEditorComponent)
@@ -26,13 +25,13 @@ export class UpdateTemplateComponent implements OnInit {
     locale: 'pt-BR',
     features: {
       textEditor: {
-        tables: true
-      }
-    }
-  }
+        tables: true,
+      },
+    },
+  };
   cardImageBase64;
   isImageSaved;
-  openPopup = true
+  openPopup = true;
   modalRef: BsModalRef;
 
   constructor(
@@ -41,35 +40,37 @@ export class UpdateTemplateComponent implements OnInit {
     private notifierService: NotifierService,
     private router: Router,
     private restangular: Restangular,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) {
-    this.id = this.route.snapshot.params['id']
+    this.id = this.route.snapshot.params['id'];
 
     this.formulario = this.formBuilder.group({
       descricao: [null, Validators.required],
-      tag: ['']
-    })
+      tag: [''],
+    });
   }
 
   ngOnInit() {
-    this.restangular.all('marketing/TemplateNotificacao').get(this.id).subscribe(dados => {
-      this.designJson = JSON.parse(dados.data.designJson);
-      this.formulario = this.formBuilder.group({
-        descricao: [dados.data.descricao, Validators.required],
-        tag: ['']
-      })
-    })
-
+    this.restangular
+      .all('marketing/TemplateNotificacao')
+      .get(this.id)
+      .subscribe((dados) => {
+        this.designJson = JSON.parse(dados.data.designJson);
+        this.formulario = this.formBuilder.group({
+          descricao: [dados.data.descricao, Validators.required],
+          tag: [dados.data.tag],
+        });
+      });
   }
 
   editorReady(event) {
-    const that = this
+    const that = this;
     this.setDesign();
     this.exportHtml();
 
     this.emailEditor.editor.addEventListener('design:updated', (updates) => {
       this.exportHtml();
-    })
+    });
 
     this.emailEditor.editor.setMergeTags({
       comissao_leiloeiro: {
@@ -150,34 +151,35 @@ export class UpdateTemplateComponent implements OnInit {
       },
     });
 
-    this.emailEditor.editor.registerCallback('image', function(file, done) {
+    this.emailEditor.editor.registerCallback('image', function (file, done) {
       const imagem: any = {
         base64: '',
         tipo: '',
         nome: '',
-      }
+      };
 
       const reader = new FileReader();
       reader.onload = (e: any) => {
         const image = new Image();
         image.src = e.target.result;
-        image.onload = rs => {
+        image.onload = (rs) => {
           const imgBase64Path = e.target.result;
           that.cardImageBase64 = imgBase64Path;
           that.isImageSaved = true;
-          imagem.base64 = imgBase64Path
-          imagem.nome = file.attachments[0].name
-          imagem.tipo = file.attachments[0].type
+          imagem.base64 = imgBase64Path;
+          imagem.nome = file.attachments[0].name;
+          imagem.tipo = file.attachments[0].type;
 
-          that.restangular.all('marketing/imageTemplate')
-          .post(imagem)
-          .subscribe(a => {
-            done({ progress: 100, url: a.data.url })
-          })
-        }
-      }
+          that.restangular
+            .all('marketing/imageTemplate')
+            .post(imagem)
+            .subscribe((a) => {
+              done({ progress: 100, url: a.data.url });
+            });
+        };
+      };
       reader.readAsDataURL(file.attachments[0]);
-    })
+    });
   }
 
   exportHtml() {
@@ -186,14 +188,14 @@ export class UpdateTemplateComponent implements OnInit {
       this.template = {
         designJson: JSON.stringify(data.design),
         codigoHtml: data.html,
-        descricao: this.formulario.value.descricao
-      }
+        descricao: this.formulario.value.descricao,
+      };
       this.salvar = false;
-    })
+    });
   }
 
   openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, { class: 'modal-lg'});
+    this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
   }
 
   save() {
@@ -201,17 +203,23 @@ export class UpdateTemplateComponent implements OnInit {
 
     if (!this.formulario.valid) {
       Object.keys(this.formulario.controls).forEach((campo) => {
-        const controle = this.formulario.get(campo)
-        controle.markAsTouched()
-      })
-      this.notifierService.notify('error', 'Preencha todos os campos obrigatórios');
-      this.salvar = false
+        const controle = this.formulario.get(campo);
+        controle.markAsTouched();
+      });
+      this.notifierService.notify(
+        'error',
+        'Preencha todos os campos obrigatórios'
+      );
+      this.salvar = false;
       return false;
     }
 
     if (!this.template) {
-      this.notifierService.notify('error', 'Template vazio, adicione mais elementos');
-      this.salvar = false
+      this.notifierService.notify(
+        'error',
+        'Template vazio, adicione mais elementos'
+      );
+      this.salvar = false;
       return false;
     }
 
@@ -220,20 +228,28 @@ export class UpdateTemplateComponent implements OnInit {
       codigoHtml: this.template.codigoHtml,
       descricao: this.formulario.value.descricao,
       templateNotificacaoId: this.id,
-      tag: this.formulario.value.tag
-    }
+      tag: this.formulario.value.tag,
+    };
 
-    this.restangular.all('Marketing/TemplateNotificacao').customPUT(this.template, this.id).subscribe(a => {
-      this.salvar = false;
-      this.notifierService.notify('success', 'Template atualizado com sucesso');
-      this.router.navigate(['template'])
-    },
-    error => {
-      this.salvar = false;
-      this.notifierService.notify('error', 'Erro ao atualizar o template!');
-    });
+    this.restangular
+      .all('Marketing/TemplateNotificacao')
+      .customPUT(this.template, this.id)
+      .subscribe(
+        (a) => {
+          this.salvar = false;
+          this.notifierService.notify(
+            'success',
+            'Template atualizado com sucesso'
+          );
+          this.router.navigate(['template']);
+        },
+        (error) => {
+          this.salvar = false;
+          this.notifierService.notify('error', 'Erro ao atualizar o template!');
+        }
+      );
 
-    this.modalRef.hide()
+    this.modalRef.hide();
   }
 
   setDesign() {
@@ -241,15 +257,17 @@ export class UpdateTemplateComponent implements OnInit {
   }
 
   verificaValidTouched(campo) {
-    this.formulario.controls [campo].valueChanges.subscribe ((val) => {
-      if (String (val) === 'NaN') {
-        this.formulario.controls [campo].setValue(null);
+    this.formulario.controls[campo].valueChanges.subscribe((val) => {
+      if (String(val) === 'NaN') {
+        this.formulario.controls[campo].setValue(null);
       }
     });
-    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+    return (
+      !this.formulario.get(campo).valid && this.formulario.get(campo).touched
+    );
   }
 
   aplicaCssErro(campo) {
-    return{ 'has-error': this.verificaValidTouched(campo) }
+    return { 'has-error': this.verificaValidTouched(campo) };
   }
 }
