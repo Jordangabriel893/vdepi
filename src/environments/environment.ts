8e0135b74e2432df6a96b3defdac63f8a279c367
Jordangabriel893 <jordan.gabrielbc@gmail.com>
2023-12-05 12:23:43 -0300

    Removendo informações

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,6 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
+
 export const environment = {
-  production: false,
-  apiUrl: "https://hml.id.eblonline.com.br",
-  apiDados: "https://hml.api.eblonline.com.br",
-  // apiUrl: 'https://localhost:5001',
-  // apiDados: 'https://localhost:4001',
+  apiUrl: 'https://localhost:5001',
+  apiDados: 'https://localhost:4001',
   siteUrl: 'https://localhost:8080',
-  // http://localhost:4200/#/assinatura/94e0a4f1-cf84-4c17-89d5-a869f609b7cc
 };
